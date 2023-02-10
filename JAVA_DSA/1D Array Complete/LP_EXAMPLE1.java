7a5c278a045205f66909b7c7b6813742fbc5eb78
lpcoderpw <123803291+lpcoderpw@users.noreply.github.com>
2023-02-10 21:28:16 +0530

    Create LP_EXAMPLE1.java

---
+++ JAVA_DSA/1D Array Complete/LP_EXAMPLE1.java
@@ -0,0 +1,9 @@
+public class CODE1 {
+    public static void main (String[] args) throws java.lang.Exception
+	{
+		String[] states= {"Delhi", "Chandigarh", "Pune"};
+        // print elements to the console:
+        for(int i = 0; i<states.length; i++)
+           System.out.print(states[i]+", ");         
+   }
+}
