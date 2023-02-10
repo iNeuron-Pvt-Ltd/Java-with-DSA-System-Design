ad104491b827b2bb07c96194c584d50a97115890
lpcoderpw <123803291+lpcoderpw@users.noreply.github.com>
2023-02-10 21:32:57 +0530

    Create ASS_CODE3.java

---
+++ JAVA_DSA/1D Array Complete/ASS_CODE3.java
@@ -0,0 +1,11 @@
+public class CODE12 {
+    public static void main(String[] args) {
+        int[] arr = { 10, 7, -5, 8, 9, 0, -4 };
+        int max = Integer.MIN_VALUE;
+        for (int val : arr) {
+            max = Math.max(max, val);
+        }
+        System.out.print("Largest in given array is " + max);
+
+    }
+}
