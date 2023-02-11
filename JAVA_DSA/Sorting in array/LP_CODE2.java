b061f7b1ca2dfacd89604d9f31357481be95ed21
lpcoderpw <123803291+lpcoderpw@users.noreply.github.com>
2023-02-11 16:39:30 +0530

    Update LP_CODE2.java

--- JAVA_DSA/Sorting in array/LP_CODE2.java
+++ JAVA_DSA/Sorting in array/LP_CODE2.java
@@ -1,40 +1,41 @@
-#include <bits/stdc++.h>
-using namespace std;
 // 0-based indexing used here 
 
-void insertionSort(vector<int> &a)
-{
-    int n = a.size(); 
- 
-    for (int i = 1; i < n; i++) { 
-        int j = i;
- 
-        // Insert a[i] into sorted left part 0..i-1
-        while (j > 0 && a[j] < a[j - 1]) {
- 
-            // Swap a[j] and a[j-1]
-            swap(a[j], a[j - 1]);
- 
-            // Decrement j by 1
-            j --;
+import java.io.*;
+import java.util.*;
+
+public class Sort {
+    public static void insertionSort(int[] a) {
+        int n = a.length;
+
+        for (int i = 1; i < n; i++) {
+            int j = i;
+
+            // Insert a[i] into sorted left part 0..i-1
+            while (j > 0 && a[j] < a[j - 1]) {
+
+                // Swap a[j] and a[j-1]
+                int temp = a[j];
+                a[j] = a[j - 1];
+                a[j - 1] = temp;
+
+                // Decrement j by 1
+                j--;
+            }
         }
     }
-} 
 
- 
-// Driver Code
-int main(){ 
-	int n;
-	cout<<"Enter the size of array"<<endl;
-	cin>>n;
-	vector<int>v(n);
-	cout<<"Enter the elements"<<endl;
-	for(int i=0;i<n;i++){
-		cin>>v[i];
- 	}
-	insertionSort(v);
-	cout<<"Array after sorting"<<endl;
-	for(int i=0;i<n;i++){
-	    cout<<v[i]<<" "; 
-	 }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of array");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter the elements of array");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        insertionSort(arr);
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
 }
