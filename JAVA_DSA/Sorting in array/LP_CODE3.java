588d52e2eb9726d3ef939e0416419bdc0f0b06c7
lpcoderpw <123803291+lpcoderpw@users.noreply.github.com>
2023-02-11 16:59:00 +0530

    Update LP_CODE3.java

--- JAVA_DSA/Sorting in array/LP_CODE3.java
+++ JAVA_DSA/Sorting in array/LP_CODE3.java
@@ -1,44 +1,45 @@
-include < bits / stdc++.h >
-using namespace std;
+import java.io.*;
+import java.util.*;
 
-// 0 based indexing used 
-void selectionSort(vector < int > & a) {
-    int n = a.size();
+public class Sort {
+    // 0 based indexing used
+    public static void selectionSort(int[] a) {
+        int n = a.length;
 
-    for (int i = 0; i < n - 1; i++) // i represents the current index
-    {
-        // Find the minimum element in unsorted part of the array
-        int min_index = i;
-        for (int j = i + 1; j < n; j++) {
-            if (a[j] < a[min_index])
-                min_index = j;
+        for (int i = 0; i < n - 1; i++)
+        // i represents the current index
+        {
+
+            // Find the minimum element in unsorted part of the array
+            int min_index = i;
+            for (int j = i + 1; j < n; j++) {
+                if (a[j] < a[min_index])
+                    min_index = j;
+            }
+
+            // Swap the found minimum element with the current element
+            if (min_index != i) {
+                int temp = a[min_index];
+                a[min_index] = a[i];
+                a[i] = temp;
+            }
         }
-
-        // Swap the found minimum element with the current element 
-        if (min_index != i)
-            swap(a[min_index], a[i]);
     }
-}
 
-
-
-
-
-
-
-// Driver Code
-int main() {
-    int n;
-    cout << "Enter the size of array" << endl;
-    cin >> n;
-    vector < int > v(n);
-    cout << "Enter the elements" << endl;
-    for (int i = 0; i < n; i++) {
-        cin >> v[i];
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of array");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter the elements of array");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        selectionSort(arr);
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
     }
-    selectionSort(v);
-    cout << "Array after sorting" << endl;
-    for (int i = 0; i < n; i++) {
-        cout << v[i] << " ";
-    }
+
+
 }
