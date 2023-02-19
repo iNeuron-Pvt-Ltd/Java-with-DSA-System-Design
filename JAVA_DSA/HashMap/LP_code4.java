f2b6f0d885e4544f05d2cc497bce31c1567c1bf6
lpcoderpw <123803291+lpcoderpw@users.noreply.github.com>
2023-02-19 14:24:30 +0530

    Create LP_code4.java

---
+++ JAVA_DSA/HashMap/LP_code4.java
@@ -0,0 +1,40 @@
+import java.util.*;
+import java.util.Map;
+import java.io.*;
+  
+public class HashMapClass {
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the String: ");  
+        
+        String s=sc.nextLine();
+
+
+        HashMap<Character, Integer> hm = new HashMap<>();
+
+        for(int i=0;i<s.length(); i++ ){
+            if(hm.containsKey(s.charAt(i))) {
+                hm.put(s.charAt(i), hm.get(s.charAt(i))+1);
+            }
+            else
+                hm.put(s.charAt(i), 1);
+        }       
+
+        //Now we will check first character whose frequency is 1
+
+        Integer answer = -1;
+        for(int i=0;i<s.length(); i++) {
+
+            if(hm.get(s.charAt(i)) == 1){
+
+                System.out.println("The first non repeating character index is: "+ (i));
+                answer = 1;
+                break;
+            }
+        }
+        if(answer == -1)
+            System.out.println("No character is found: -1");
+
+    }
+}
