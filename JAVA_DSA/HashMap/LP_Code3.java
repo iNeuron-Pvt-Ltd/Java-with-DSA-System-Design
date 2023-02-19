b08c650cc154d27421dc7489b929cc56713d18e6
lpcoderpw <123803291+lpcoderpw@users.noreply.github.com>
2023-02-19 14:01:54 +0530

    Create LP_Code3.java

---
+++ JAVA_DSA/HashMap/LP_Code3.java
@@ -0,0 +1,59 @@
+import java.util.*;
+import java.util.Map;
+import java.io.*;
+  
+public class HashMapClass {
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of elements you want to store: ");  
+        //reading the number of elements from the that we want to enter  
+        int n=sc.nextInt();  
+        //creates an array in the memory of length 10  
+        int[] array = new int[10];  
+        System.out.println("Enter the elements of the array: ");  
+        for(int i=0; i<n; i++) {  
+            //reading array elements from the user   
+            array[i]=sc.nextInt();  
+        }  
+
+        System.out.println("Enter the target element of the array: ");  
+
+        //Enter target element
+        int target = sc.nextInt();
+
+
+        HashMap<Integer, Integer> hm= new HashMap<>();
+
+        //Store every element in Map
+
+        for(int i=0;i<n;i++){
+            hm.put(array[i], i);
+        } 
+
+        int[] answer = new int[2];
+
+        for(int i=0; i<n ;i++){
+
+            //If the current element is equal to target.
+            if(array[i] == target && hm.containsKey(0)){
+                answer[0] = i;
+                answer[1] = hm.get(0);
+                break;
+            }
+            //if we check the difference is present in map
+            else if(hm.containsKey(target - array[i])){
+                if(hm.get(target - array[i]) > i){
+                    //THis case is to check for same elements case. array[i] == target - array[i]
+                    answer[0] = i;
+                    answer[1] = hm.get(target - array[i]);
+                    break;
+                }
+            }
+        }
+
+        System.out.println("The output array is: ["+ answer[0]+ "," + answer[1] + "]");
+        
+        
+    }
+}
