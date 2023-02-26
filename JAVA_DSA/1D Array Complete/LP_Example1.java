df5ea1fe6fbac960da906db4c288d0a1903fb697
lpcoderpw <123803291+lpcoderpw@users.noreply.github.com>
2023-02-26 10:54:51 +0530

    Update and rename LP_EXAMPLE1.java to LP_Example1.java

---
+++ JAVA_DSA/1D Array Complete/LP_Example1.java
@@ -0,0 +1,12 @@
+public class LP_Example1 {
+    public static void main(String[] args) {
+        String[] states = {
+            "Delhi",
+            "Chandigarh",
+            "Pune"
+        };
+        // print elements to the console:
+        for (int i = 0; i < states.length; i++)
+            System.out.print(states[i] + ", ");
+    }
+}
