38d48c5dd2c3d77608143d9fa1ade545b528e0eb
lpcoderpw <123803291+lpcoderpw@users.noreply.github.com>
2023-02-26 11:08:18 +0530

    Update and rename LP_Code1.java to CheckPalindrome.java

---
+++ JAVA_DSA/1D Array Complete/CheckPalindrome.java
@@ -0,0 +1,16 @@
+public class CheckPalindrome {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 2, 1};
+        int n = 5;
+        int flag = 0;
+        for (int i = 0; i < n / 2; i++) {
+            if (arr[i] != arr[n - i - 1]) {
+                flag = 1;
+                System.out.println("The given array is not a palindrome.");
+            }
+        }
+
+        if (flag == 0) System.out.println("The given array is a palindrome.");
+
+    }
+}
