d6b055eec3b0a4150ec9448ff550af4c6651c26a
lpcoderpw <123803291+lpcoderpw@users.noreply.github.com>
2023-02-26 11:13:11 +0530

    Update LP_Code3.java

--- JAVA_DSA/1D Array Complete/LP_Code3.java
+++ JAVA_DSA/1D Array Complete/LP_Code3.java
@@ -1,11 +1,10 @@
-public class Main
-{
-	public static void main(String[] args) {
-		int []arr = {1,2,3,5};
-		int n = 4;
-	    int sum = ((n + 1) * (n + 2)) / 2;
-        for (int i = 0; i < n; i++)sum -= arr[i];
-        
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 5};
+        int n = 4;
+        int sum = ((n + 1) * (n + 2)) / 2;
+        for (int i = 0; i < n; i++) sum -= arr[i];
+
         System.out.println("The missing numer is : " + sum);
-	}
+    }
 }
