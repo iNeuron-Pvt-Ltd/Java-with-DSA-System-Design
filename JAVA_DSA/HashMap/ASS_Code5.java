c18918cbc7ba601415a3bd8efcd4f4901066743c
lpcoderpw <123803291+lpcoderpw@users.noreply.github.com>
2023-04-08 13:27:31 +0530

    Update ASS_Code5.java

--- JAVA_DSA/HashMap/ASS_Code5.java
+++ JAVA_DSA/HashMap/ASS_Code5.java
@@ -8,39 +8,39 @@ public class HashMapClass {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the RansomNote String: ");  
         
-        String s=sc.nextLine();
+        String ransomNote=sc.nextLine();
 
         System.out.print("Enter the Magazine String: ");  
         
-        String r=sc.nextLine();
+        String magazine=sc.nextLine();
 
 
-        HashMap<Character, Integer> hm1 = new HashMap<>();
+        HashMap<Character, Integer> ransomNoteHashMap = new HashMap<>();
 
-        for(int i=0;i<s.length(); i++ ){
-            if(hm1.containsKey(s.charAt(i))) {
-                hm1.put(s.charAt(i), hm1.get(s.charAt(i))+1);
+        for(int i=0;i<ransomNote.length(); i++ ){
+            if(ransomNoteHashMap.containsKey(ransomNote.charAt(i))) {
+                ransomNoteHashMap.put(ransomNote.charAt(i), ransomNoteHashMap.get(ransomNote.charAt(i))+1);
             }
             else
-                hm1.put(s.charAt(i), 1);
+                ransomNoteHashMap.put(ransomNote.charAt(i), 1);
         }     
 
-        HashMap<Character, Integer> hm2 = new HashMap<>();
+        HashMap<Character, Integer> magazineHashMap = new HashMap<>();
 
-        for(int i=0;i<r.length(); i++ ){
-            if(hm2.containsKey(r.charAt(i))) {
-                hm2.put(r.charAt(i), hm2.get(r.charAt(i))+1);
+        for(int i=0;i<magazine.length(); i++ ){
+            if(magazineHashMap.containsKey(magazine.charAt(i))) {
+                magazineHashMap.put(magazine.charAt(i), magazineHashMap.get(magazine.charAt(i))+1);
             }
             else
-                hm2.put(r.charAt(i), 1);
+                magazineHashMap.put(magazine.charAt(i), 1);
         }     
 
         
 
         Integer answer = -1;
-        for(Map.Entry<Character, Integer> e: hm1.entrySet()) {
+        for(Map.Entry<Character, Integer> e: ransomNoteHashMap.entrySet()) {
 
-            if(e.getValue() > hm2.get(e.getKey())){
+            if(e.getValue() > magazineHashMap.get(e.getKey())){
 
                 System.out.println("false");
                 answer = 1;
