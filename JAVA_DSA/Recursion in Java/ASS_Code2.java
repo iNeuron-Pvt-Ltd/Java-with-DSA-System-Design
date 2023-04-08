bf6b0c1bbfb8275da5dbc7bb7fcac34c88071170
lpcoderpw <123803291+lpcoderpw@users.noreply.github.com>
2023-04-08 12:24:59 +0530

    Update ASS_Code2.java

--- JAVA_DSA/Recursion in Java/ASS_Code2.java
+++ JAVA_DSA/Recursion in Java/ASS_Code2.java
@@ -2,16 +2,16 @@ import java.io.*;
 import java.util.*;
 
 public class Main
-{   static int go(int n  , int i){
-    if(i == n + 1)return 0;
-    if(i%2 == 0)return go(n , i + 1) - i;
-    else return go(n , i + 1) + i;
+{   static int go(int lastNaturalNumber  , int index){
+    if(index == lastNaturalNumber + 1)return 0;
+    if(index % 2 == 0)return go(lastNaturalNumber , index + 1) - i;
+    else return go(lastNaturalNumber , index + 1) + i;
 }
 	public static void main(String[] args) {
 	   
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        System.out.println(go(n,1));
+        int lastNaturalNumber = sc.nextInt();
+        System.out.println(go(lastNaturalNumber,1));
    
 
 	}
