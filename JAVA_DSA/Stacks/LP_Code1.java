199e29197413ed8b2e07716119b06e0e57dc731e
lpcoderpw <123803291+lpcoderpw@users.noreply.github.com>
2023-04-08 13:55:04 +0530

    Update LP_Code1.java

--- JAVA_DSA/Stacks/LP_Code1.java
+++ JAVA_DSA/Stacks/LP_Code1.java
@@ -2,26 +2,30 @@ import java.io.*;
 import java.util.*;
 import java.util.Stack; 
 
-class Main {
-	public static void main(String[] args){
+class Test {
+    public static void main(String[] args){
 
-		// Default initialization of Stack
-		Stack<Integer> stack1 = new Stack();
+        // Default initialization of Stack
+        Stack<Integer> stack1 = new Stack();
 
-		// pushing the elements
-		stack1.push(1);
-		stack1.push(2);
-		stack1.push(3);
+        // pushing the elements
+        stack1.push(1);
+        stack1.push(2);
+        
+        //popping the Element
+
+        stack1.pop();
+        
+        System.out.println("The peek element of the stack is : " + stack1.peek());
+
+        stack1.push(3);
         stack1.push(5);
         
         //printing the peek element
         System.out.println("The peek element of the stack is : " + stack1.peek());
         
-        //popping the Elements
+        //popping the Element        
         
-        stack1.pop();
-        
-        System.out.println("The peek element of the stack is : " + stack1.peek());
         
         stack1.pop();
         
@@ -29,8 +33,8 @@ class Main {
       
         
         
-		// Printing the Stack Elements
-		
-		System.out.println("The stack elements are as follows : " + stack1);
-	}
+        // Printing the Stack Elements
+        
+        System.out.println("The stack elements are as follows : " + stack1);
+    }
 }
