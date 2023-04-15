3f61484447fabe5683d24a5063d4b9e1f2000904
lpcoderpw <123803291+lpcoderpw@users.noreply.github.com>
2023-04-15 12:26:44 +0530

    Update LP_Code2.java

--- JAVA_DSA/DP-1/LP_Code2.java
+++ JAVA_DSA/DP-1/LP_Code2.java
@@ -1,22 +1,22 @@
 public class Main{
     static void climbingStairs(int n){
-    	// Declare an array to store Fibonacci numbers. 
-    	int dp[] = new int[n+1]; 
-    	int i;
-    	dp[0] = 1;  //here ith index denotes i+1 that are i+1 stairs available
-    	dp[1] = 1; 
-    	
-    	for (i = 2; i <= n; i++){
-    	// Add the previous 2 numbers in the series	and store it 
-    		dp[i] = dp[i-1] + dp[i-2];
-    	}
-    	
-        System.out.println("The possible ways are : " + dp[n]);
+        // Declare an array to store Fibonacci numbers. 
+        int ways[] = new int[n+1]; 
+        int i;
+        ways[0] = 1;  //here ith index denotes i+1 that are i+1 stairs available
+        ways[1] = 1; 
+        
+        for (i = 2; i <= n; i++){
+        // Add the previous 2 numbers in the series and store it 
+            ways[i] = ways[i-1] + ways[i-2];
+        }
+        
+        System.out.println("The possible ways are : " + ways[n]);
     }
-	
-	public static void main (String args[]){
-		int n = 5;
-		climbingStairs(n);
-	}
+    
+    public static void main (String args[]){
+        int n = 5;
+        climbingStairs(n);
+    }
 };
 
