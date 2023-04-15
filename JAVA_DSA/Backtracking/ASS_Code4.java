faa49b4599148fc1a10954f250ea2b4a88a392d5
lpcoderpw <123803291+lpcoderpw@users.noreply.github.com>
2023-04-15 11:45:59 +0530

    Update ASS_Code4.java

--- JAVA_DSA/Backtracking/ASS_Code4.java
+++ JAVA_DSA/Backtracking/ASS_Code4.java
@@ -3,17 +3,17 @@ import java.util.*;
 
 public class Main {
    static int n;
-   public static boolean solve(int n, int target, int a[], int i, int product) {
+   public static boolean findProduct(int n, int target, int a[], int i, int product) {
        if (i == n) return (product == target);
        boolean answer = false;
 
        product *= a[i];
        
-       answer |= solve(n, target, a, i + 1, product);
+       answer |= findProduct(n, target, a, i + 1, product);
        
        product /= a[i];
        
-       answer |= solve(n, target, a, i + 1, product);
+       answer |= findProduct(n, target, a, i + 1, product);
 
        return answer;
    }
@@ -28,6 +28,6 @@ public class Main {
        for (int i = 0; i < n; i++) {
            a[i] = sc.nextInt();
        }
-       System.out.println((solve(n, target, a, 0, 1) ? "YES" : "NO"));
+       System.out.println((findProduct(n, target, a, 0, 1) ? "YES" : "NO"));
    }
 }
