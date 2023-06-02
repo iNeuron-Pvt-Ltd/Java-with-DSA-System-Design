4020f8da4caff2b89a01aafd8bb0ffd1d4c13731
lpcoderpw <123803291+lpcoderpw@users.noreply.github.com>
2023-06-02 13:38:47 +0530

    Update ASS_Code4.java
    Replaced Assignment question

--- JAVA_DSA/Divide & Conquer/ASS_Code4.java
+++ JAVA_DSA/Divide & Conquer/ASS_Code4.java
@@ -1,29 +1,41 @@
-import java.util.*;
-
-class Main{
-    static boolean isPossible(Integer a[], int b[], int n, int k){
-    	// Sort the array a[] in decreasing order.
-    	Arrays.sort(a, Collections.reverseOrder());
-    
-    	// Sort the array b[] in increasing order.
-    	Arrays.sort(b);
-    
-    	// Checking condition on each index.
-    	for (int i = 0; i < n; i++)
-    	if (a[i] + b[i] < k)
-    		return false;
-    
-    	return true;
+public class Main{
+    public static int reversePairs(int[] nums) {
+		if (nums == null || nums.length < 2) return 0;
+        return mergesort(nums, 0, nums.length-1);
     }
+	
+	public static int mergesort(int[] nums, int low, int high){
+		if (low >= high) return 0;
+		int mid = low + (high - low) / 2;
+		int count = mergesort(nums, low, mid) + mergesort(nums, mid+1, high);
+		for (int i = low, j = mid+1; i <= mid && j <= high;){
+			if (nums[i] > (long) nums[j] * 2){
+                count += mid - i + 1;
+                j++;
+            }
+            else i++;
+		}
+		
+		merge(nums, low, high);
+		return count;
+	}
+	
+	public static void merge(int[] nums, int low, int high){
+		int mid = low + (high - low) / 2;
+		int[] arr = new int[high - low + 1];
+		
+		int i = low, j = mid + 1, k = 0;
+		while (k < arr.length){
+			int num1 = i > mid ? Integer.MAX_VALUE : nums[i];
+			int num2 = j > high ? Integer.MAX_VALUE : nums[j];
+			
+			arr[k++] = num1 <= num2 ? nums[i++] : nums[j++];
+		}
+		
+		for (int p = 0; p < arr.length; p++) nums[p+low] = arr[p];
+	}
     public static void main(String[] args) {
-    	Integer a[] = {2, 1, 3, 5, 1};
-    	int b[] = {7, 8, 9 , 2 , 6};
-    	int k = 10;
-    	int n = a.length;
-    
-    	if (isPossible(a, b, n, k))
-    	System.out.print("Yes");
-    	else
-    	System.out.print("No");
-    }
+        int a[] = {1,3,2,3,1};
+	    System.out.println("The number of reverse pairs are : " + reversePairs(a));
+	}
 }
