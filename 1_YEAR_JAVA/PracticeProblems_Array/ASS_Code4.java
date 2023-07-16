57d6e93f0dedc9b7783bbde956fb37718e76b4b7
lpcoderpw <123803291+lpcoderpw@users.noreply.github.com>
2023-07-16 20:45:34 +0530

    Update ASS_Code4.java

--- 1_YEAR_JAVA/PracticeProblems_Array/ASS_Code4.java
+++ 1_YEAR_JAVA/PracticeProblems_Array/ASS_Code4.java
@@ -1,33 +1,31 @@
+import java.io.*;
+import java.util.*;
 import java.util.Scanner;
-public class Main {
-    public static void main(String[] args) {
-        Scanner scn = new Scanner(System.in);
-        System.out.println("Enter the length of array");
-        int n = scn.nextInt();
-        int[] arr = new int[n];
-        System.out.println("Enter the elements of array");
-        for (int i = 0; i < n; i++) {
-            arr[i] = scn.nextInt();
+
+
+public class Main
+{  public static int findOddOccuring(int[] arr)
+    {
+        int xor = 0;
+        for (int i: arr) {
+            xor = xor ^ i;
         }
-        for (int i = 1; i < n; i++) {
-            int j = i;
-            // Insert a[i] into sorted left part 0..i-1
-            while (j > 0 && arr[j] < arr[j - 1]) {
-                // Swap a[j] and a[j-1]
-                int temp = arr[j];
-                arr[j] = arr[j - 1];
-                arr[j - 1] = temp;
-                // Decrement j by 1
-                j--;
-            }
-        }
-        int si;
-        if(n % 2 == 0){
-            si = n/2;
-        }else{
-            si = n/2 + 1;
-        }
-        for (int i = si; i < n; i++)
-            System.out.print(arr[i] + " ");
+        return xor;
     }
+	public static void main(String[] args) {
+		int n;  
+        Scanner sc=new Scanner(System.in);  
+        System.out.print("Enter the number of elements you want to store: ");  
+        //reading the number of elements from the that we want to enter  
+        n=sc.nextInt();  
+        //creates an array in the memory of length 10  
+        int[] array = new int[10];  
+        System.out.println("Enter the elements of the array: ");  
+        for(int i=0; i<n; i++)  
+        {  
+        //reading array elements from the user   
+            array[i]=sc.nextInt();  
+        }
+     System.out.println("The odd occurring element is " + findOddOccuring(array));
+	}
 }
