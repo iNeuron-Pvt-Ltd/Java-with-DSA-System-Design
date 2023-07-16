8c1bf53b4044fed2d16f812523ed03cb0f3ee8b6
lpcoderpw <123803291+lpcoderpw@users.noreply.github.com>
2023-07-16 22:07:33 +0530

    Create LP_Code4.java

---
+++ 1_YEAR_JAVA/Strings/LP_Code4.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+
+public class Main{ 
+    public static void main(String args[]){ 
+        StringBuffer s1=new StringBuffer("Physics "); 
+        s1.append("Wallah");                    
+     //now original string is changed 
+        System.out.println(s1);               
+    }
+}
+
