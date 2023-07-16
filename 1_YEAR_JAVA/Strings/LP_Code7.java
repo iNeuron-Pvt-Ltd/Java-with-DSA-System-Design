e69e3c9cc6c656c6891643f55c1ed57efea4fa6b
lpcoderpw <123803291+lpcoderpw@users.noreply.github.com>
2023-07-16 22:10:54 +0530

    Create LP_Code7.java

---
+++ 1_YEAR_JAVA/Strings/LP_Code7.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+
+public class Main{ 
+    public static void main(String args[]){ 
+        StringBuffer s1=new StringBuffer("Physics"); 
+        s1.reverse();                     
+        //now original string is changed 
+        System.out.println(s1);               
+    }
+}
+
+
+
+
