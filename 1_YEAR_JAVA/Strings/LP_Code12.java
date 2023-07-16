ddc3909789542f4619b9ac0afacfbad1d3e8e08f
lpcoderpw <123803291+lpcoderpw@users.noreply.github.com>
2023-07-16 22:17:53 +0530

    Create LP_Code12.java

---
+++ 1_YEAR_JAVA/Strings/LP_Code12.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main
+   {
+       public static void main(String args[])
+       {
+            StringBuffer s1 = new StringBuffer("Physics");
+            s1.insert(2,"Wallah");  
+            System.out.println(s1);
+       }
+   }
+
+
