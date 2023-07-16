b4fe848aa850773ee7cd851733976718c73d2ef4
lpcoderpw <123803291+lpcoderpw@users.noreply.github.com>
2023-07-16 20:10:15 +0530

    Create ASS_CODE4.java

---
+++ 1_YEAR_JAVA/Sorting/ASS_CODE4.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Enter the length of array");
+        int n = scn.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter the elements of array");
+        for (int i = 0; i < n; i++) {
+            arr[i] = scn.nextInt();
+        }
+        for (int i = 1; i < n; i++) {
+            int j = i;
+            // Insert a[i] into sorted left part 0..i-1
+            while (j > 0 && arr[j] < arr[j - 1]) {
+                // Swap a[j] and a[j-1]
+                int temp = arr[j];
+                arr[j] = arr[j - 1];
+                arr[j - 1] = temp;
+                // Decrement j by 1
+                j--;
+            }
+        }
+        int si;
+        if(n % 2 == 0){
+            si = n/2;
+        }else{
+            si = n/2 + 1;
+        }
+        for (int i = si; i < n; i++)
+            System.out.print(arr[i] + " ");
+    }
+}
