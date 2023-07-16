dca0b342a8646437ec12da050cefdf1390b70f32
lpcoderpw <123803291+lpcoderpw@users.noreply.github.com>
2023-07-16 22:19:59 +0530

    Create LP_Code14.java

---
+++ 1_YEAR_JAVA/Strings/LP_Code14.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main {  
+       public static void main(String[] args) {  
+        StringBuilder sb1 = new StringBuilder("Physics Wallah");  
+        System.out.println("string = "+sb1);  
+        sb1.delete(8,14);
+        System.out.println("String after modification= "+sb1);  
+        
+   }
+}
