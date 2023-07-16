8a08415d39a76c192bf58e0eab374e2faa134bc0
lpcoderpw <123803291+lpcoderpw@users.noreply.github.com>
2023-07-16 11:47:01 +0530

    Create LP_Code1.java

---
+++ 1_YEAR_JAVA/StacksAndQueues/LP_Code1.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class StacksAndQueues {
+
+    public static void main(String[] args) {
+
+        stack s = new stack();
+        s.push(6);
+        s.push(3);
+        s.push(7);
+        System.out.println("Top of the stack before deleting any element " + s.top());
+        System.out.println("Size of the stack before deleting any element " + s.size());
+        System.out.println("The element deleted is " + s.pop());
+        System.out.println("Size of the stack after deleting an element " + s.size());
+        System.out.println("Top of the stack after deleting an element " + s.top());
+    }
+}
+
+class stack {
+    int size = 10000;
+    int arr[] = new int[size];
+    int top = -1;
+    void push(int x) {
+        top++;
+        arr[top] = x;
+    }
+    int pop() {
+        int x = arr[top];
+        top--;
+        return x;
+    }
+    int top() {
+        return arr[top];
+    }
+    int size() {
+        return top + 1;
+    }
+}
