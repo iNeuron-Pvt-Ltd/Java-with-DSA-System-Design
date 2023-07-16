30e2915af0308149cb3ec9e2932f88fb4cb4b1cb
lpcoderpw <123803291+lpcoderpw@users.noreply.github.com>
2023-07-16 20:45:07 +0530

    Update ASS_Code3.java

--- 1_YEAR_JAVA/PracticeProblems_Array/ASS_Code3.java
+++ 1_YEAR_JAVA/PracticeProblems_Array/ASS_Code3.java
@@ -1,46 +1,26 @@
 import java.io.*;
 import java.util.*;
-
-public class Sort {
-    // 0 based indexing used
-    public static void selectionSort(int[] a) {
-        int n = a.length;
-
-        for (int i = 0; i < n - 1; i++)
-        // i represents the current index
-        {
-
-            // Find the maximum element in unsorted part of the array
-            int max_index = i;
-            for (int j = i + 1; j < n; j++) {
-                if (a[j] > a[max_index])
-                    max_index = j;
+public class Main{ 
+  
+    public static boolean isPerfectSquare(int num) {
+       if(num == 1) return true ;  // edge case
+       long start = 0 ;   // Take Long as the inputs have large value
+       long end = num /2 ;
+        while(start <= end ){
+            long mid = start + ( end - start ) / 2 ;
+            if (mid * mid > num)   end = mid - 1 ; // Mid is greater than the squareroot of the number
+            else if (mid * mid < num) start = mid + 1 ;  // Mid is less than our required number
+            else  return true;     // we found our squareroot number
             }
-
-            // Swap the found maximum element with the current element
-            if (max_index != i) {
-                int temp = a[max_index];
-                a[max_index] = a[i];
-                a[i] = temp;
-            }
-        }
+        return false ;
     }
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the size of array");
-        int n = sc.nextInt();
-        int[] arr = new int[n];
-        System.out.println("Enter the elements of array");
-        for (int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
-        }
-        selectionSort(arr);
-        for (int i = 0; i < n; i++) {
-            System.out.print(arr[i] + " ");
-        }
-        System.out.print("\n");
-    }
-
-
+    public static void main(String args[]){
+        int m;
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the number : ");
+        m=sc.nextInt();      
+       
+        System.out.println("The given number is a perfect square: " + isPerfectSquare(m));
+    }     
 }
