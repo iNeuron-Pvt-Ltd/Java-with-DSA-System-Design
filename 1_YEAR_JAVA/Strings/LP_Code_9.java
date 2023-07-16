6d4eeb92175ecb44944dd1882186838f758758f8
lpcoderpw <123803291+lpcoderpw@users.noreply.github.com>
2023-07-16 22:14:32 +0530

    Create LP_Code_9.java

---
+++ 1_YEAR_JAVA/Strings/LP_Code_9.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main{ 
+    public static void main(String args[]){ 
+        StringBuilder s1=new StringBuilder("Physics "); 
+        s1.append("Wallah");                    
+     //now original string is changed 
+        System.out.println(s1);               
+    }
+}
+
