6ac929153617d107f47f5407b98b296416646475
lpcoderpw <123803291+lpcoderpw@users.noreply.github.com>
2023-07-16 20:41:28 +0530

    Update LP_Code4.java

--- 1_YEAR_JAVA/PracticeProblems_Array/LP_Code4.java
+++ 1_YEAR_JAVA/PracticeProblems_Array/LP_Code4.java
@@ -1,49 +1,24 @@
-import java.util.Arrays;
-
-class CountingSort {
-  void countSort(int array[], int size) {
-    int[] output = new int[size + 1];
-
-    // Find the largest element of the array
-    int max = array[0];
-    for (int i = 1; i < size; i++) {
-      if (array[i] > max)
-        max = array[i];
-    }
-    int[] count = new int[max + 1];
-
-    // Initialize count array with all zeros.
-    for (int i = 0; i < max; ++i) {
-      count[i] = 0;
-    }
-
-    // Store the count of each element
-    for (int i = 0; i < size; i++) {
-      count[array[i]]++;
-    }
-
-    // Store the cummulative count of each array
-    for (int i = 1; i <= max; i++) {
-      count[i] += count[i - 1];
-    }
-
-    // Find the index of each element of the original array in count array, and place the elements in output array
-    for (int i = size - 1; i >= 0; i--) {
-      output[count[array[i]] - 1] = array[i];
-      count[array[i]]--;
-    }
-
-    // Copy the sorted elements into original array
-    for (int i = 0; i < size; i++) {
-      array[i] = output[i];
-    }
-  }
-  public static void main(String args[]) {
-    int[] data = { 4, 2, 2, 8, 3, 3, 1 };
-    int size = data.length;
-    CountingSort cs = new CountingSort();
-    cs.countSort(data, size);
-    System.out.println("Sorted Array in Ascending Order: ");
-    System.out.println(Arrays.toString(data));
-  }
-}
+public class Main {  
+    private static int lower_bound(int[] nums, int target) {
+        int low = 0, high = nums.length - 1;
+        while(low < high) {
+            int mid = low + (high - low) / 2;
+            if(nums[mid] < target)
+                low = mid + 1;
+            else
+                high = mid;
+        }
+        return nums[low] == target? low : -1;
+    }   
+    public static void main(String argvs[]) {  
+     
+        int arr[] = {1,2,4,5,8,19,20};  
+        int len = arr.length;  
+        System.out.println("For the input array: ");  
+        for(int  i = 0; i < len; i++) System.out.print(arr[i] + " ");  
+        System.out.println();
+        int tgt = 8;
+        System.out.println("The given target value is :" + tgt);
+        System.out.println("The lower bound of the given target is: " + lower_bound(arr , tgt));
+    }  
+}  
