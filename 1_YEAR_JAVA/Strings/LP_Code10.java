1c90296b9377983cb541b71245d9e6544be0b9d3
lpcoderpw <123803291+lpcoderpw@users.noreply.github.com>
2023-07-16 22:15:38 +0530

    Create LP_Code10.java

---
+++ 1_YEAR_JAVA/Strings/LP_Code10.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Main {  
+    public static void main(String[] args) {  
+        StringBuilder str1 = new StringBuilder("physics");  
+        System.out.println("string = " + str1);  
+        // reversing of stringbuilder  
+        System.out.println("reverse = " + str1.reverse());  
+    } 
+}
