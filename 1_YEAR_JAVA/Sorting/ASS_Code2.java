fa8d3ced1cf3d504f415bcb07e24e1140efc0f69
lpcoderpw <123803291+lpcoderpw@users.noreply.github.com>
2023-07-16 20:13:33 +0530

    Create ASS_Code2.java

---
+++ 1_YEAR_JAVA/Sorting/ASS_Code2.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.*;
+public class Sort {
+    // 0 based indexing used
+    public static void bubbleSort(int[] a) {
+        int n = a.length;
+
+        for (int i = 0; i < n; i++) {
+            boolean flag = false;
+
+            for (int j = 0; j < n - i - 1; j++) {
+                if (a[j] < a[j + 1]) {
+                    flag = true;
+                    // swap the values of a[j] and a[j+1]
+                    int temp = a[j];
+                    a[j] = a[j + 1];
+                    a[j + 1] = temp;
+                }
+            }
+            // No Swapping happened, array is sorted
+            if (!flag) {
+                return;
+            }
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of array");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter the elements of array");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        bubbleSort(arr);
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
