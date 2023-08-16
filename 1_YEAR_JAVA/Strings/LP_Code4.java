425c24c52c69449f026810a3e2684f439808fe0e
lpcoderpw <123803291+lpcoderpw@users.noreply.github.com>
2023-08-16 17:26:15 +0530

    Update LP_Code4.java

--- 1_YEAR_JAVA/Strings/LP_Code4.java
+++ 1_YEAR_JAVA/Strings/LP_Code4.java
@@ -3,7 +3,7 @@ import java.util.*;
 
 public class Main{ 
     public static void main(String args[]){ 
-        StringBuffer s1=new StringBuffer("Physics "); 
+        StringBuilder s1=new StringBuilder("Physics "); 
         s1.append("Wallah");                    
      //now original string is changed 
         System.out.println(s1);               
