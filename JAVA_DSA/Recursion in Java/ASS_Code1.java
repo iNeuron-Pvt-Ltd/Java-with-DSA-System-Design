f2f9d4a4acafd482dc057ae6aac53fb8f0b710dd
lpcoderpw <123803291+lpcoderpw@users.noreply.github.com>
2023-09-20 09:52:14 +0530

    Update ASS_Code1.java

--- JAVA_DSA/Recursion in Java/ASS_Code1.java
+++ JAVA_DSA/Recursion in Java/ASS_Code1.java
@@ -1,22 +1,27 @@
 import java.io.*;
 import java.util.*;
 
-
-public class Main
-{   static int func(int n)
+class Hanoi
 {
-    if (n == 0)
-    return 0;
-    return (n % 10 + func(n / 10));
-}
-	public static void main(String[] args) {
-		System.out.println("Enter the number");
-		Scanner sc = new Scanner(System.in);
-		int n=sc.nextInt();
-		int ans = func(n);
-		System.out.println(" Sum of digits in " + n
-       + " is " + ans );
-	}
+    // Java recursive function to solve tower of hanoi puzzle
+    static void towerOfHanoi(int n, char from_rod, char to_rod, char aux_rod)
+    {
+        if (n == 1)
+        {
+            System.out.println("Move disk 1 from rod " +  from_rod + " to rod " + to_rod);
+            return;
+        }
+        towerOfHanoi(n-1, from_rod, aux_rod, to_rod);
+        System.out.println("Move disk " + n + " from rod " +  from_rod + " to rod " + to_rod);
+        towerOfHanoi(n-1, aux_rod, to_rod, from_rod);
+    }
+     
+    //  Driver method
+    public static void main(String args[])
+    {
+        int n = 4; // Number of disks
+        towerOfHanoi(n, \'A\', \'C\', \'B\');  // A, B and C are names of rods
+    }
 }
 
  
